a8688de9ee389c7a0ae1c4249932f9ec5919b4a4
ARATI-UPARE <aratiupare@gmail.com>
2020-03-12 14:33:58 +0530

    ADDED:Code optimized

--- dayInbetweenMonth.sh
+++ dayInbetweenMonth.sh
@@ -5,20 +5,15 @@
 d=$1
 m=$2
 
-if [[ $m -ge 3 && $m -le 6 ]]
+if [[ $m -eq 3 && $d -ge 20 && $d -le 31 ]]
 then
-		if [[ $m -eq 3 && $d -ge 20 && $d -le 31 ]]
-		then
-				echo true
-		elif [[ $m -eq 6 && $d -le 20 && $d -ge 1 ]]
-		then
-				echo true
-		elif [[ $m -ne 3 && $m -ne 6 && $d -ge 1 && $d -le 31 ]]
-		then
-				echo true
-		else
-				echo false
-		fi
+		echo true
+elif [[ $m -eq 6 && $d -le 20 && $d -ge 1 ]]
+then
+		echo true
+elif [[ $m -gt 3 && $m -lt 6 && $d -ge 1 && $d -le 31 ]]
+then
+		echo true
 else
 		echo false
 fi
